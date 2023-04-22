8a9f77c640e949b3f5c328b3835177b7e70d4ada
altcode255 <anonte3p5usu@gmail.com>
2023-04-21 18:18:31 -0700

    Reformat using black

--- namelessbot.py
+++ namelessbot.py
@@ -6,9 +6,14 @@ import os
 directory = os.path.realpath(os.path.dirname(__file__))
 
 with open(directory + "/creds.json") as credentials:
-	keys = json.load(credentials)
+    keys = json.load(credentials)
 
-client = tweepy.Client(consumer_key=keys["consumer_key"], consumer_secret=keys["consumer_secret"], access_token=keys["access_token"], access_token_secret=keys["access_token_secret"])
+client = tweepy.Client(
+    consumer_key=keys["consumer_key"],
+    consumer_secret=keys["consumer_secret"],
+    access_token=keys["access_token"],
+    access_token_secret=keys["access_token_secret"],
+)
 
 gc = pygsheets.authorize(service_file=directory + "/gsheets.json")
 sh = gc.open("The Sheet of Series that Start with S")
@@ -16,7 +21,9 @@ wks = sh[1]
 
 
 def getStats() -> str:
-	return wks.get_value("B2", value_render=pygsheets.ValueRenderOption.UNFORMATTED_VALUE)
+    return wks.get_value(
+        "B2", value_render=pygsheets.ValueRenderOption.UNFORMATTED_VALUE
+    )
 
 
 client.create_tweet(text=getStats())
