f2a16be1394f5a8228b0bef76b2a89964b0b7e71
altcode255 <anonte3p5usu@gmail.com>
2023-09-16 20:39:06 -0700

    Wait for internet

--- s_counter.bat
+++ s_counter.bat
@@ -1,2 +1,7 @@
+:ping
+ping 1.2.3.4 -n 1 -w 1000 > nul
+set target=www.google.com
+ping %target% -n 1 | find "TTL="
+if errorlevel==1 goto ping
 taskkill /f /IM pythonw3.11.exe
 start "" pythonw s_counter.pyw
