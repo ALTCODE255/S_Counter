3cc2621e1317c40970462a9800a3971bb43749d8
altcode255 <anonte3p5usu@gmail.com>
2023-10-19 17:25:07 -0700

    Format with black

--- db_functions.py
+++ db_functions.py
@@ -21,7 +21,9 @@ def getCount(col: str) -> int:
 def incCounter(col: str, num: int):
     sqliteConnection = sqlite3.connect("counter.db")
     cursor = sqliteConnection.cursor()
-    cursor.execute(f"UPDATE S_Counter SET {col} = {col} + 1 WHERE Date = (SELECT MAX(Date) FROM S_Counter)")
+    cursor.execute(
+        f"UPDATE S_Counter SET {col} = {col} + 1 WHERE Date = (SELECT MAX(Date) FROM S_Counter)"
+    )
     sqliteConnection.commit()
     cursor.close()
     sqliteConnection.close()
@@ -31,27 +33,33 @@ def incCounter(col: str, num: int):
 def getStatistics() -> tuple[dict]:
     sqliteConnection = sqlite3.connect("counter.db")
     cursor = sqliteConnection.cursor()
-    cursor.execute('''SELECT
+    cursor.execute(
+        """SELECT
                         SUM(Shuuen), ROUND(AVG(Shuuen), 2), MAX(Shuuen),
                         SUM(Sonic), ROUND(AVG(Sonic), 2), MAX(Sonic)
-                    FROM S_Counter''')
+                    FROM S_Counter"""
+    )
     values_tuple = cursor.fetchone()
     cursor.close()
     sqliteConnection.close()
     return (
-            {"Word": "Shuuen",
-             "SUM": values_tuple[0],
-             "AVG": values_tuple[1],
-             "MAX": values_tuple[2]},
-            {"Word": "Sonic",
-             "SUM": values_tuple[3],
-             "AVG": values_tuple[4],
-             "MAX": values_tuple[5]}
-        )
+        {
+            "Word": "Shuuen",
+            "SUM": values_tuple[0],
+            "AVG": values_tuple[1],
+            "MAX": values_tuple[2],
+        },
+        {
+            "Word": "Sonic",
+            "SUM": values_tuple[3],
+            "AVG": values_tuple[4],
+            "MAX": values_tuple[5],
+        },
+    )
 
 
 def addNewDayRow():
-    today = (datetime.now() + timedelta(minutes=5)).strftime('%Y-%m-%d')
+    today = (datetime.now() + timedelta(minutes=5)).strftime("%Y-%m-%d")
     sqliteConnection = sqlite3.connect("counter.db")
     cursor = sqliteConnection.cursor()
     try:
@@ -66,11 +74,17 @@ def addNewDayRow():
 
 def updateHomeAssistant():
     load_dotenv()
-    requests.packages.urllib3.disable_warnings() 
-    with Client(os.getenv("INTERNAL_IP"), os.getenv("HA_TOKEN"),  verify_ssl=False) as client:
+    requests.packages.urllib3.disable_warnings()
+    with Client(
+        os.getenv("INTERNAL_IP"), os.getenv("HA_TOKEN"), verify_ssl=False
+    ) as client:
         counter = client.get_domain("input_number")
-        counter.set_value(value=getCount("Shuuen"), entity_id="input_number.shuuen_counter")
-        counter.set_value(value=getCount("Sonic"), entity_id="input_number.sonic_counter")
+        counter.set_value(
+            value=getCount("Shuuen"), entity_id="input_number.shuuen_counter"
+        )
+        counter.set_value(
+            value=getCount("Sonic"), entity_id="input_number.sonic_counter"
+        )
 
 
 if __name__ == "__main__":
