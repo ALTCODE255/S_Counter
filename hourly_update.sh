6b22f9ef74daf0e677338b177807e405589cc4c5
altcode255 <anonte3p5usu@gmail.com>
2023-10-26 11:34:35 -0700

    Update gist only if changed

--- hourly_update.sh
+++ hourly_update.sh
@@ -1,10 +1,12 @@
 export $(xargs <.env)
 sqlite3 -markdown counter.db "SELECT * FROM S_Counter ORDER BY Date Desc" > S_Counter.md
 new_content=$(sed ':a;N;$!ba;s/\n/\\n/g' S_Counter.md)
-curl -L \
-  -X PATCH \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GH_PAT" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/gists/f674d02b89b93cdeb51ea782e03f06ff \
-  -d '{"description":"","files":{"S_Counter.md":{"content":"'"$new_content"'"}}}'
+if [ "$(curl https://gist.githubusercontent.com/ALTCODE255/f674d02b89b93cdeb51ea782e03f06ff/raw/03f3513aedbf6e3beaf6549e9020cbbb015bd8da/S_Counter.md/ 2>/dev/null | head -3)" != "$(head -3 S_Counter.md)" ]; then
+    curl -L \
+    -X PATCH \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GH_PAT" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/gists/f674d02b89b93cdeb51ea782e03f06ff \
+    -d '{"description":"","files":{"S_Counter.md":{"content":"'"$new_content"'"}}}'
+fi
