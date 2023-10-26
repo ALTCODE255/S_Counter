1b976ce7a40450cb7d3d24b31042e6bd96c2adf6
altcode255 <anonte3p5usu@gmail.com>
2023-10-26 11:36:56 -0700

    Update with correct raw url

--- hourly_update.sh
+++ hourly_update.sh
@@ -1,7 +1,7 @@
 export $(xargs <.env)
 sqlite3 -markdown counter.db "SELECT * FROM S_Counter ORDER BY Date Desc" > S_Counter.md
 new_content=$(sed ':a;N;$!ba;s/\n/\\n/g' S_Counter.md)
-if [ "$(curl https://gist.githubusercontent.com/ALTCODE255/f674d02b89b93cdeb51ea782e03f06ff/raw/03f3513aedbf6e3beaf6549e9020cbbb015bd8da/S_Counter.md/ 2>/dev/null | head -3)" != "$(head -3 S_Counter.md)" ]; then
+if [ "$(curl https://gist.githubusercontent.com/ALTCODE255/f674d02b89b93cdeb51ea782e03f06ff/raw/ 2>/dev/null | head -3)" != "$(head -3 S_Counter.md)" ]; then
     curl -L \
     -X PATCH \
     -H "Accept: application/vnd.github+json" \
