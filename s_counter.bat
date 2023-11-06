dd071edbb89967ac3cb396aa6d5d9dcd98207279
altcode255 <anonte3p5usu@gmail.com>
2023-11-05 16:30:26 -0800

    Forward output to nul

--- s_counter.bat
+++ s_counter.bat
@@ -2,7 +2,7 @@
 :ping
 ping 1.2.3.4 -n 1 -w 1000 > nul
 set target=www.google.com
-ping %target% -n 1 | find "TTL="
+ping %target% -n 1 | find "TTL=" > nul
 if errorlevel==1 goto ping
-taskkill /f /IM pythonw3.11.exe
-start "" pythonw bg_script.pyw
+taskkill /f /IM pythonw3.11.exe > nul
+start "" pythonw bg_script.pyw > nul
