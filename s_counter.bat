1790e1506e8a0a22cbbc40fc6d1c81a5feeee28c
altcode255 <anonte3p5usu@gmail.com>
2024-02-05 21:55:28 -0800

    Remove batch file

--- s_counter.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-:ping
-ping 1.2.3.4 -n 1 -w 1000 > nul
-set target=www.google.com
-ping %target% -n 1 | find "TTL=" > nul
-if errorlevel==1 goto ping
-start "" pythonw bg_script.pyw > nul
