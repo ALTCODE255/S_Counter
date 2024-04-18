c74ef64c70a42e4c9fe83fac57f5e231504a1349
altcode255 <anonte3p5usu@gmail.com>
2024-04-18 03:19:01 -0700

    Add authentication

--- increment.pyw
+++ increment.pyw
@@ -1,14 +1,14 @@
 import sys
 import os
 
-import urllib.request
+import requests
 from dotenv import load_dotenv
 from plyer import notification
 
 
 def updateCount(inc_col: str):
-    response = urllib.request.urlopen(os.getenv("URL") + inc_col.lower())
-    count = response.read().decode("utf-8")
+    response = requests.get(os.getenv("URL") + inc_col.lower(), auth=(os.getenv("USER"), os.getenv("PASSWORD")))
+    count = response.content.decode("utf-8")
     notification.notify(
         title="Counted!", message=f"{inc_col}: {count}", timeout=1
     )
--- receiver.py
+++ receiver.py
@@ -1,25 +1,38 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import dotenv_values, load_dotenv
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
 
 from db_functions import getCount, incCounter
 
+
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+load_dotenv()
+config = dotenv_values()
+app.config.from_mapping(config)
 
 
-@app.route('/sonic')
+@auth.verify_password
+def verify_password(username, password):
+    if username == app.config["USER"] and password == app.config["PASSWORD"]:
+        return username
+
+
+@app.route("/sonic")
+@auth.login_required
 def incrementSonic():
     incCounter("Sonic", 1)
     return str(getCount("Sonic"))
 
 
-@app.route('/shuuen')
+@app.route("/shuuen")
+@auth.login_required
 def incrementShuuen():
     incCounter("Shuuen", 1)
     return str(getCount("Shuuen"))
 
 
-if __name__ == '__main__':
-    load_dotenv()
+if __name__ == "__main__":
     app.run(port=os.getenv("PORT"), host=os.getenv("HOST"))
