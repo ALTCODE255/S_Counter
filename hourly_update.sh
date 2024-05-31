6b95043f5917c7fd907e0e338326f4159e5e338e
altcode255 <anonte3p5usu@gmail.com>
2024-05-30 20:48:20 -0700

    Add space

--- hourly_update.sh
+++ hourly_update.sh
@@ -1,4 +1,4 @@
-export $(xargs <.env)
+export $(xargs < .env)
 sqlite3 -markdown counter.db "SELECT * FROM S_Counter ORDER BY Date Desc" > S_Counter.md
 new_content=$(sed ':a;N;$!ba;s/\n/\\n/g' S_Counter.md)
 if [ "$(curl https://gist.githubusercontent.com/ALTCODE255/f674d02b89b93cdeb51ea782e03f06ff/raw/ 2>/dev/null | head -3)" != "$(head -3 S_Counter.md)" ]; then
