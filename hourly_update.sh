d873f79b0edaf58d11077b41f3af5c172f0569d6
altcode255 <anonte3p5usu@gmail.com>
2024-07-10 18:28:46 -0700

    Quote argument

--- hourly_update.sh
+++ hourly_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-export $(xargs < .env)
+export "$(xargs < .env)"
 new_content=$(sqlite3 -markdown counter.db "SELECT * FROM S_Counter ORDER BY Date Desc")
 if [[ "$(gh gist view f674d02b89b93cdeb51ea782e03f06ff | head -3)" != "$(echo "$new_content" | head -3)" ]]; then
     echo "$new_content" | gh gist edit f674d02b89b93cdeb51ea782e03f06ff -f S_Counter.md -
