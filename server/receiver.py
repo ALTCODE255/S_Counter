fb1a221a04b9bdbdaca9b74df330b3bd5c1ef174
altcode255 <anonte3p5usu@gmail.com>
2024-10-07 10:19:12 -0700

    Add route

--- server/receiver.py
+++ server/receiver.py
@@ -8,7 +8,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from db_functions import getCount, incCounter
 
 app = Flask(__name__)
-auth = HTTPTokenAuth(scheme='Bearer')
+auth = HTTPTokenAuth(scheme="Bearer")
 load_dotenv()
 config = dotenv_values()
 app.config.from_mapping(config)
@@ -22,14 +22,20 @@ def verify_token(token):
 
 @app.route("/increment", methods=["POST"])
 @auth.login_required
-def s_counter():
-    if request.method == 'POST':
-        word = request.headers["inc-col"]
-        incCounter(word, 1)
-        return str(getCount(word))
+def increment():
+    word = request.headers["inc-col"]
+    incCounter(word, 1)
+    return str(getCount(word))
+
+
+@app.route("/get_count/<col>", methods=["GET"])
+@auth.login_required
+def get_count(col):
+    return str(getCount(col))
 
 
 if __name__ == "__main__":
     from waitress import serve
+
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     serve(app, port=os.getenv("PORT"), host=os.getenv("HOST"))
